8185fbe9b745e50d5daafa8953b24e7fc27fec0c
galaraz <jvictor.si.2013@gmail.com>
2021-10-21 16:55:30 -0300

    arrumando datas

---
+++ 34/Dia-03/aulaAovivo/34-3/profile_iterator.py
@@ -0,0 +1,30 @@
+from _collections_abc import Iterable, Iterator
+
+
+class ProfileIterator(Iterator): # nosso iterator
+    def __init__(self, friends):
+        self.__friends = friends
+        self.__index = 0
+
+    def __next__(self):
+        try:
+            current_value = self.__friends[self.__index]
+        except IndexError:
+            # Exceção lançada quando não há mais elementos a serem iterados
+            raise StopIteration()
+        else:
+            self.__index += 1
+            return current_value
+
+class SocialNetwork(Iterable): # quem vai fazer a iteração, com todos os dados que o __next percorreu
+    def __init__(self, friends):
+        self.friends = friends
+
+    def __iter__(self):
+        return ProfileIterator(self.friends) # chamada dados da classe Profileiterator
+
+
+network = SocialNetwork(["Ana", "Pedro", "Maria", "Miguel"])
+for profile in network:
+    print(profile)
+
